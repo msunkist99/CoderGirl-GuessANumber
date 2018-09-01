aae8de84d372a74560bfddd9e57978212b2a5f4b
Scott Kuhl <scott@kuhl.ws>
2018-09-01 00:35:53 -0500

    Add project files.

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
