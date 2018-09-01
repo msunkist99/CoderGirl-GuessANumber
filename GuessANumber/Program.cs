41be5db5bfc1a1a570797fb7773ead6232270928
Scott Kuhl <scott@kuhl.ws>
2018-09-01 00:42:57 -0500

    Initial project.

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -2,11 +2,16 @@
 
 namespace GuessANumber
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a number between 1 and 10: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // If they enter 8, output “You Win!”. Otherwise, output “You Lose.”. Do this using an inline if statement.
+
+            Console.ReadLine();
         }
     }
 }
