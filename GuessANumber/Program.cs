87ce07aab03477fe3a526f1582731dd0b41cee2d
msunkist99 <msunkist99@gmail.com>
2019-01-25 17:59:28 -0600

    2019/01/25 add inline If statement and output

--- GuessANumber/Program.cs
+++ GuessANumber/Program.cs
@@ -10,6 +10,11 @@ namespace GuessANumber
             int number = int.Parse(Console.ReadLine());
 
             // If they enter 8, output “You Win!”. Otherwise, output “You Lose.”. Do this using an inline if statement.
+            string outputLine;
+
+            outputLine = (number == 8) ? "You Win!" : "You Lose.";
+
+            Console.WriteLine(outputLine);
 
             Console.ReadLine();
         }
